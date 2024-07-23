8f6f95700ee3a9aa699f75c20f47c2aeae069762
rithusinshith <rithu.raj@3plearning.com>
2024-07-23 16:10:29 +1000

    Added the base URL

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+
+    baseUrl : 'https://petstore.swagger.io/v2',
+    specPattern : "cypress/e2e",
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
